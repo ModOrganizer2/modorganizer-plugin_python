36022774e3325ab432deed78b814ddfad1b18315
Mikaël Capelle <capelle.mikael@gmail.com>
2020-05-03 20:17:14 +0200

    Add methods to utils to register map, vector and set.

--- src/runner/pythonutils.h
+++ src/runner/pythonutils.h
@@ -5,14 +5,16 @@
 
 namespace utils {
 
+  namespace bpy = boost::python;
+
   template <class Map>
   struct map_to_python {
     static PyObject* convert(const Map& map) {
-      boost::python::dict result;
+      bpy::dict result;
       for (auto& entry : map) {
-        result[boost::python::object{ entry.first }] = boost::python::object{ entry.second };
+        result[bpy::object{ entry.first }] = bpy::object{ entry.second };
       }
-      return boost::python::incref(result.ptr());
+      return bpy::incref(result.ptr());
     }
   };
 
@@ -26,15 +28,97 @@ namespace utils {
       return PyDict_Check(objPtr) ? objPtr : nullptr;
     }
 
-    static void construct(PyObject* objPtr, boost::python::converter::rvalue_from_python_stage1_data* data) {
-      void* storage = ((boost::python::converter::rvalue_from_python_storage<Map>*)data)->storage.bytes;
+    static void construct(PyObject* objPtr, bpy::converter::rvalue_from_python_stage1_data* data) {
+      void* storage = ((bpy::converter::rvalue_from_python_storage<Map>*)data)->storage.bytes;
       Map* result = new (storage) Map();
-      boost::python::dict source(boost::python::handle<>(boost::python::borrowed(objPtr)));
-      boost::python::list keys = source.keys();
-      int len = boost::python::len(keys);
+      bpy::dict source(bpy::handle<>(bpy::borrowed(objPtr)));
+      bpy::list keys = source.keys();
+      int len = bpy::len(keys);
       for (int i = 0; i < len; ++i) {
-        boost::python::object pyKey = keys[i];
-        (*result)[boost::python::extract<key_type>(pyKey)] = boost::python::extract<value_type>(source[pyKey]);
+        bpy::object pyKey = keys[i];
+        (*result)[bpy::extract<key_type>(pyKey)] = bpy::extract<value_type>(source[pyKey]);
+      }
+
+      data->convertible = storage;
+    }
+  };
+
+  template <class Container>
+  struct container_to_python_list {
+    static PyObject* convert(const Container& container) {
+      bpy::list pyList;
+
+      try {
+        for (auto& item : container)
+          pyList.append(item);
+      }
+      catch (const bpy::error_already_set&) {
+        reportPythonError();
+      }
+
+      return bpy::incref(pyList.ptr());
+    }
+  };
+
+
+  template <class Container>
+  struct container_from_python_list {
+
+    using value_type = typename Container::value_type;
+
+    static void* convertible(PyObject* objPtr) {
+      if (PySequence_Check(objPtr)) return objPtr;
+      return nullptr;
+    }
+
+    static void construct(PyObject* objPtr, bpy::converter::rvalue_from_python_stage1_data* data) {
+      void* storage = ((bpy::converter::rvalue_from_python_storage<Container>*)data)->storage.bytes;
+      Container* result = new (storage) Container();
+      bpy::list source(bpy::handle<>(bpy::borrowed(objPtr)));
+      int length = bpy::len(source);
+      for (int i = 0; i < length; ++i) {
+        result->push_back(bpy::extract<value_type>(source[i]));
+      }
+
+      data->convertible = storage;
+    }
+  };
+
+  template <class Container>
+  struct set_to_python {
+    static PyObject* convert(const Container& container) {
+      bpy::list pyList;
+
+      try {
+        for (auto& item : container)
+          pyList.append(item);
+      }
+      catch (const bpy::error_already_set&) {
+        reportPythonError();
+      }
+
+      return bpy::incref(pyList.ptr());
+    }
+  };
+
+
+  template <class Container>
+  struct set_from_python {
+
+    using value_type = typename Container::value_type;
+
+    static void* convertible(PyObject* objPtr) {
+      if (PySequence_Check(objPtr)) return objPtr;
+      return nullptr;
+    }
+
+    static void construct(PyObject* objPtr, bpy::converter::rvalue_from_python_stage1_data* data) {
+      void* storage = ((bpy::converter::rvalue_from_python_storage<Container>*)data)->storage.bytes;
+      Container* result = new (storage) Container();
+      bpy::list source(bpy::handle<>(bpy::borrowed(objPtr)));
+      int length = bpy::len(source);
+      for (int i = 0; i < length; ++i) {
+        result->insert(bpy::extract<value_type>(source[i]));
       }
 
       data->convertible = storage;
@@ -42,15 +126,32 @@ namespace utils {
   };
 
   template <class Map>
-  void register_map() {
-
-    boost::python::to_python_converter<Map, map_to_python<Map>>();
-
-    boost::python::converter::registry::push_back(
+  void register_associative_container() {
+    bpy::to_python_converter<Map, map_to_python<Map>>();
+    bpy::converter::registry::push_back(
       &map_from_python<Map>::convertible
       , &map_from_python<Map>::construct
-      , boost::python::type_id<Map>());
+      , bpy::type_id<Map>());
   };
+
+  template <class Container>
+  void register_set_container() {
+    bpy::to_python_converter<Container, set_to_python<Container>>();
+    bpy::converter::registry::push_back(
+      &set_from_python<Container>::convertible
+      , &set_from_python<Container>::construct
+      , bpy::type_id<Container>());
+  };
+
+  template <class Container>
+  void register_sequence_container() {
+    bpy::to_python_converter<Container, container_to_python_list<Container>>();
+    bpy::converter::registry::push_back(
+      &container_from_python_list<Container>::convertible
+      , &container_from_python_list<Container>::construct
+      , bpy::type_id<Container>());
+  };
+
 
 
 }
