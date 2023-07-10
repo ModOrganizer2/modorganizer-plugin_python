19b86bc67bdb16332bd64afdaff63fd067667e9f
Mikaël Capelle <capelle.mikael@gmail.com>
2023-07-10 21:57:59 +0200

    Fix issue with unloading single .py file and fix tests.

--- tests/runner/test_lifetime.cpp
+++ tests/runner/test_lifetime.cpp
@@ -7,38 +7,42 @@
 
 TEST(Lifetime, Plugins)
 {
-    const auto plugins_folder = QString(std::getenv("PLUGIN_DIR"));
+    const auto plugins_folder = std::filesystem::path(std::getenv("PLUGIN_DIR"));
 
     auto runner = mo2::python::createPythonRunner();
     runner->initialize();
 
     {
-        const auto objects = runner->load(plugins_folder + "/dummy-iplugin.py");
+        const auto objects =
+            runner->load("dummy_iplugin", plugins_folder / "dummy-iplugin.py");
 
         // we found one plugin
-        EXPECT_EQ(objects.size(), 1);
+        ASSERT_EQ(objects.size(), 1);
 
         // check that deleting the object actually destroys it
         bool destroyed = false;
-        QObject::connect(objects[0], &QObject::destroyed, [&destroyed]() {
+        QObject::connect(objects[0][0], &QObject::destroyed, [&destroyed]() {
             destroyed = true;
         });
-        delete objects[0];
+        delete objects[0][0];
         EXPECT_EQ(destroyed, true);
+
+        runner->unload("dummy_iplugin", plugins_folder / "dummy-iplugin.py");
     }
 
     // same things but with a parent
     {
         QObject* dummy_parent = new QObject();
-        const auto objects    = runner->load(plugins_folder + "/dummy-iplugin.py");
+        const auto objects =
+            runner->load("dummy_iplugin", plugins_folder / "dummy-iplugin.py");
 
         // we found one plugin
-        EXPECT_EQ(objects.size(), 1);
-        objects[0]->setParent(dummy_parent);
+        ASSERT_EQ(objects.size(), 1);
+        objects[0][0]->setParent(dummy_parent);
 
         // check that deleting the object actually destroys it
         bool destroyed = false;
-        QObject::connect(objects[0], &QObject::destroyed, [&destroyed]() {
+        QObject::connect(objects[0][0], &QObject::destroyed, [&destroyed]() {
             destroyed = true;
         });
         delete dummy_parent;
