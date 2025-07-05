b7f9a377d5173f0514e6527743b3e4b5c07ce6c5
Mikaël Capelle <capelle.mikael@gmail.com>
2025-07-05 15:38:54 +0200

    Add a few tests to check binding for IFileTree glob and walk.

--- tests/python/test_filetree.py
+++ tests/python/test_filetree.py
@@ -1,3 +1,5 @@
+from typing import TypeAlias, cast
+
 import mobase
 
 import mobase_tests.filetree as m
@@ -34,3 +36,51 @@ def test_filetype():
 
     assert m.is_file(FT.FILE_OR_DIRECTORY & ~FT.DIRECTORY)
     assert not m.is_directory(FT.FILE_OR_DIRECTORY & ~FT.DIRECTORY)
+
+
+_tree_values: TypeAlias = list["str | tuple[str, _tree_values]"]
+
+
+def make_tree(
+    values: _tree_values, root: mobase.IFileTree | None = None
+) -> mobase.IFileTree:
+    if root is None:
+        root = cast(mobase.IFileTree, mobase.private.makeTree())  # type: ignore
+
+    for value in values:
+        if isinstance(value, str):
+            root.addFile(value)
+        else:
+            sub_tree = root.addDirectory(value[0])
+            make_tree(value[1], sub_tree)
+
+    return root
+
+
+def test_walk():
+    tree = make_tree(
+        [("a", []), ("b", ["u", "v"]), "c.x", "d.y", ("e", [("q", ["c.t", ("p", [])])])]
+    )
+
+    assert {"a", "b", "b/u", "b/v", "c.x", "d.y", "e", "e/q", "e/q/c.t", "e/q/p"} == {
+        e.path("/") for e in tree.walk()
+    }
+
+    entries: list[str] = []
+    for e in tree.walk():
+        if e.name() == "e":
+            break
+        entries.append(e.path("/"))
+    assert {"a", "b", "b/u", "b/v"} == set(entries)
+
+
+def test_glob():
+    tree = make_tree(
+        [("a", []), ("b", ["u", "v"]), "c.x", "d.y", ("e", [("q", ["c.t", ("p", [])])])]
+    )
+
+    assert {"a", "b", "b/u", "b/v", "c.x", "d.y", "e", "e/q", "e/q/c.t", "e/q/p"} == {
+        e.path("/") for e in tree.glob("**/*")
+    }
+
+    assert {"d.y"} == {e.path("/") for e in tree.glob("**/*.y")}
